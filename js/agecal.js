80190bf07c866aec0aeeb167a72597f87678d875
fayiz7 <fayiz7.kau@gmail.com>
2017-08-08 17:40:13 +0300

    update

--- js/agecal.js
+++ js/agecal.js
@@ -1,1 +1,12 @@
-
+function agecal() {
+  var para = document.getElementById('printResult');
+  var days = document.getElementById('days');
+  var months = document.getElementById('months');
+  var years = document.getElementById('years');
+  var day = parseInt(days.value);
+  var month = parseInt(months.value);
+  var year = parseInt(years.value);
+  var currentDate = Date();
+  para.innerHTML="welcome"+currentDate;
+  para.write(currentDate);
+}
